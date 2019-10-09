f5e227619bd9af470534080c1ad43b65fda0bbcb
yeya24 <yb532204897@gmail.com>
2019-10-09 15:22:25 -0400

    update passcraker

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # passcraker
-A simple password craker
+A simple password craker.
+
+## How to use
+
+```shell script
+usage: passcraker.py [-h] [--threads THREADS] [--queue_size QUEUE_SIZE]
+                     dictionary password
+
+positional arguments:
+  dictionary            The dictionary file you want to use to crack password
+  password              The victim password file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --threads THREADS     The number of running threads when cracking passwords
+  --queue_size QUEUE_SIZE
+                        The maximum size of the queue
+```
+
+### Example
+```shell script
+chmod +x passcraker.py
+# Set parallel threads to 50 and set queue size to 200.
+./passcraker <dict_file> <passwords_file> --threads 50 --queue_size 200
+```
--- passcraker.py
+++ passcraker.py
@@ -3,28 +3,43 @@ import argparse
 from crypt import *
 from queue import Queue
 from threading import Thread
+import time
 import sys
 from collections import namedtuple
 
-q = Queue(400)
-crack = namedtuple('crack', ['secret', 'password'])
+crack = namedtuple('crack', ['user', 'secret', 'password'])
 
 
-def calculate_hash():
-    while True:
-        c = q.get()
-        fields = c.secret.split('*')
-        if crypt(c.password, fields[1][:11]) == fields[1]:
-            print("Found passwd for user {}: {} ".format(fields[0], c.password))
+# Worker class for cracking passwords.
+class CrackThread(Thread):
+    def __init__(self, queue, users, sleep_time):
+        Thread.__init__(self, daemon=True)
+        self.queue = queue
+        self.sleep_time = sleep_time
+        self.users = users
+
+    def run(self):
+        time.sleep(self.sleep_time)
+        while not self.queue.empty():
+            c = self.queue.get()
+            # Check if we already get the password of this user.
+            if not self.users[c.user]:
+                if crypt(c.password, c.secret[:11]) == c.secret:
+                    self.users[c.user] = True
+                    print("Found passwd for user {}: {} ".format(c.user, c.password))
+            self.queue.task_done()
 
 
-def main(args):
-    words = args.dictionary.readlines()
-    pass_lines = args.password.readlines()
+def main(arg):
+    threads = arg.threads
+    words = arg.dictionary.readlines()
+    pass_lines = arg.password.readlines()
+    q = Queue(arg.queue_size)
+    users = {}
+    print('Start cracking password file {} with wordlist {}'.format(args.password.name, args.dictionary.name))
 
-    for i in range(100):
-        t = Thread(target=calculate_hash)
-        t.daemon = True
+    for i in range(threads):
+        t = CrackThread(q, users, 0.5)
         t.start()
 
     try:
@@ -32,8 +47,14 @@ def main(args):
             columns = str.split(line.strip(), ":", 2)
             if columns[1] != "*":
                 for word in words:
-                    q.put(crack(secret=columns[0] + "*" + columns[1], password=word.strip()))
+                    if columns[0] not in users:
+                        users[columns[0]] = False
+                    elif users[columns[0]]:
+                        continue
+                    q.put(crack(user=columns[0], secret=columns[1], password=word.strip()))
+
         q.join()
+        print('Cracking passwords finish')
     except KeyboardInterrupt:
         sys.exit(1)
 
@@ -43,5 +64,9 @@ if __name__ == "__main__":
     parser.add_argument("dictionary", type=argparse.FileType('r'),
                         help="The dictionary file you want to use to crack password")
     parser.add_argument("password", type=argparse.FileType('r'), help="The victim password file")
+    parser.add_argument("--threads", type=int, default=10, required=False,
+                        help="The number of running threads when cracking passwords")
+    parser.add_argument("--queue_size", type=int, default=200, required=False,
+                        help="The maximum size of the queue")
     args = parser.parse_args()
     main(args)
